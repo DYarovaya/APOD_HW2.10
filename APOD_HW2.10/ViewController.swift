affad5cdc1e8ac6418dbc19bd8ec80e6a9f215aa
Darya Yarovaya <yarovayadarya@mail.ru>
2021-06-25 12:55:37 +0300

    Initial Commit

---
+++ APOD_HW2.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APOD_HW2.10
+//
+//  Created by Дарья Яровая on 25.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
